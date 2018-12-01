f710c52f582070e44fd4124e7cc4a63f164b3dba
Maciej S <starwars.maciek@gmail.com>
2018-12-01 16:28:59 +0100

    Added: button highlight sound

---
+++ Assets/Scripts/ButtonHighlight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class ButtonHighlight : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+{
+
+    public AudioClip buttonSound;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(buttonSound);
+    }
+
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Do something.
+        PlaySound();
+        Debug.Log("<color=red>Event:</color> Completed mouse highlight.");
+        
+    }
+    // When selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        // Do something.
+        Debug.Log("<color=red>Event:</color> Completed selection.");
+//        PlaySound();
+    }
+}
