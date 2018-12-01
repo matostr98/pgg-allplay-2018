adeb68d837f0f3fa7f96429429374e2d413ea080
Oskar Burdziak <rimmorn@gmail.com>
2018-12-01 19:09:44 +0100

    Auto stash before revert of "merge"

---
+++ Assets/Scripts/Vibrator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrator : MonoBehaviour {
+
+    public void Vibrate()
+    {
+        Handheld.Vibrate();
+    }
+}
